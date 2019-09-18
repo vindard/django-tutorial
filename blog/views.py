2a03eac9ff212d11da1b2a2fed6125ce332cca8a
User <17693119+vindard@users.noreply.github.com>
2019-09-18 08:51:19 -0400

    create `posts` object in `views`, pass into render & add to html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'vindard',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Sep 18, 2019'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Sep 19, 2019'
+    },
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
