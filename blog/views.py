af874002f33d154d8c07fb250be9ce377c2335f1
User <17693119+vindard@users.noreply.github.com>
2019-09-18 20:46:12 -0400

    switch posts render out to the posts from db

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'vindard',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Sep 18, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Sep 19, 2019'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
